4db954b55a18089d2f71f44111b643e16f2d8256
Mr shashika dilshan <dmritasudarsani@gmail.com>
2025-08-23 15:04:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -23,7 +23,7 @@ module.exports = {
     SESSION_ID: process.env.SESSION_ID || "BLACKWOLF-MD~TpIDSYqL#QPot4Y4CLIDAR9To1VvzarZ6SqZmXI6zsm_stMS3D9M",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
     MODE: process.env.MODE || "public",
-    AUTO_VOICE: process.env.AUTO_VOICE || "true",
+    AUTO_VOICE: process.env.AUTO_VOICE || "false",
     AUTO_STICKER: process.env.AUTO_STICKER || "true",
     AUTO_REPLY: process.env.AUTO_REPLY || "true",
     ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/5akkt2.png",
